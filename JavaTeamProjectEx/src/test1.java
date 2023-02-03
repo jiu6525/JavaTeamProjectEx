fdffe0be5d3cda809bcd1bf4dc8c57c6f30f8ba4
Jiu <jiu6525@daum.net>
2023-02-03 17:10:49 +0900

    first

---
+++ JavaTeamProjectEx/src/test1.java
@@ -0,0 +1,18 @@
+class Hello {
+
+
+
+	public static void main(String[] args) {
+
+			System.out.println("안녕하세요");
+			System.out.println("깃허브를 연습하자");
+			System.out.println("깃허브를 연습하자2");
+
+
+						}
+
+
+
+						}
+
+
