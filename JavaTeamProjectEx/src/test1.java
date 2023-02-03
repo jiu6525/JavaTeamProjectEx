e7300955bd0584d7959ef636f3ce1c3e4071efd0
Jiu <jiu6525@daum.net>
2023-02-03 17:15:18 +0900

    test2

--- JavaTeamProjectEx/src/test1.java
+++ JavaTeamProjectEx/src/test1.java
@@ -1,18 +1,11 @@
 class Hello {
-
-
-
 	public static void main(String[] args) {
-
+		
 			System.out.println("안녕하세요");
 			System.out.println("깃허브를 연습하자");
 			System.out.println("깃허브를 연습하자2");
-
-
-						}
-
-
-
-						}
+			
+			}
+}
 
 
